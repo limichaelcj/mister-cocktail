5948f12b3b3828824f60b53e7901ec95aa6a0333
Michael Li <mike830@gmail.com>
2018-11-09 17:14:42 +0900

    edit seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,20 +24,20 @@ end
 
 puts "Faking cocktails and doses..."
 
-rand(6..10).times do
+rand(12).times do
   cocktail = Cocktail.new(name: Faker::Coffee.blend_name,
                              description: Faker::Lovecraft.sentence)
   cocktail.remote_photo_url = 'https://source.unsplash.com/collection/1990254'
-  cocktail.save!
+  cocktail.save
   Ingredient.all.sample(rand(3..6)).each do |ingredient|
     dose = Dose.create(cocktail: cocktail,
                        description: Faker::Measurement.volume,
                        ingredient: ingredient)
   end
-  rand(3..6).times do
+  rand(5..10).times do
     Review.create(cocktail: cocktail,
                   rating: rand(1..5),
-                  comment: Faker::HowIMetYourMother.quote)
+                  comment: Faker::Simpsons.quote)
   end
 end
 
